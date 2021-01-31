ca60900d6cdde6ae1c1864a798bd0ce2a651d9d3
manyfeatures <shakurs@mail.ru>
2021-01-31 18:31:42 +0300

    autopep8ed

--- scrapper.py
+++ scrapper.py
@@ -38,11 +38,11 @@ class Scrapper():
 
     def get_top_block(self):
         tmp = self.page.find("div", {"class", "top-container"})
-        #print(tmp)
-        #definition
-        self.def_  = tmp.find('h1', {'class', 'headword'}).text # make method?
+        # print(tmp)
+        # definition
+        self.def_ = tmp.find('h1', {'class', 'headword'}).text  # make method?
         print(f"Word: {self.def_.upper()}")
-        #phonetics        
+        # phonetics
         print('Phonetics:')
         phone = tmp.find(class_='phonetics')
         # transcriptions
@@ -53,9 +53,9 @@ class Scrapper():
 
     def print_grammar_label(self, def_):
         # If there is a grammar in the line. What does it mean?
-        try:  
-            print(def_.find('span', {'class', 'grammar'}).get_text(), end = ' ')
-        except:
+        try:
+            print(def_.find('span', {'class', 'grammar'}).get_text(), end=' ')
+        except BaseException:
             print(end='')
 
     def print_usage_label(self, def_):
@@ -64,13 +64,13 @@ class Scrapper():
             # this can be ambigous and can retieve example's tag "cf"
             # This is why we use loop to not get into the examples tags
             for child in def_.findChildren(recursive=False):
-                #print('inside')
-                try: # addition protection if index for some children out of range
+                # print('inside')
+                try:  # addition protection if index for some children out of range
                     if child['class'][0] == 'cf':
                         print(f' ["{child.text}"] ', end=' ')
-                except:
+                except BaseException:
                     pass
-        except:
+        except BaseException:
             print(end='')
 
     def print_label(self, def_):
@@ -79,21 +79,21 @@ class Scrapper():
             # this can be ambigous and can retieve example's tag "cf"
             # This is why we use loop to not get into the examples tags
             for child in def_.findChildren(recursive=False):
-                #print('inside')
-                try: # addition protection if index for some children out of range
+                # print('inside')
+                try:  # addition protection if index for some children out of range
                     if child['class'][0] == 'labels':
                         print(f' ["{child.text}"] ', end=': ')
-                except:
+                except BaseException:
                     pass
-        except:
+        except BaseException:
             print(end='')
 
-    def print_single_definition(self, def_):    
+    def print_single_definition(self, def_):
         self.print_grammar_label(def_)
         self.print_usage_label(def_)
-        self.print_label(def_) 
-        #definition
-        print(def_.find('span', {'class', 'def'}).get_text())  
+        self.print_label(def_)
+        # definition
+        print(def_.find('span', {'class', 'def'}).get_text())
         print()
 
     def print_examples_(self, def_):
@@ -103,24 +103,24 @@ class Scrapper():
             # We need to try to take it
             print(f"{i})", end=' ')
             try:
-                print(f' "{examp.find(class_="cf").text}" ', end=': ') 
-                print(examp.find(class_ = 'labels').text, end=' ')
-                print(examp.find(class_='x').text) # it has duplicate
+                print(f' "{examp.find(class_="cf").text}" ', end=': ')
+                print(examp.find(class_='labels').text, end=' ')
+                print(examp.find(class_='x').text)  # it has duplicate
             # If there is no specification then it is skipped
             except (NameError, AttributeError) as e:
                 try:
-                    print(examp.find(class_ = 'labels').text, end=' ')
-                    print(examp.find(class_='x').text) # it has duplicate
-                except:
+                    print(examp.find(class_='labels').text, end=' ')
+                    print(examp.find(class_='x').text)  # it has duplicate
+                except BaseException:
                     try:
-                        print(examp.find(class_='x').text) # it has duplicate
+                        print(examp.find(class_='x').text)  # it has duplicate
                     except e:
-                        #print(e)
+                        # print(e)
                         pritn(None)
 
             # How to make it with if-else without `try-except`?
             # if examp.find(class_="cf").text is not None:
-            #     print(f' "{examp.find(class_="cf").text}" ', end=': ') 
+            #     print(f' "{examp.find(class_="cf").text}" ', end=': ')
             # if examp.find(class_ = 'labels').text is not None:
             #     print(examp.find(class_ = 'labels').text, end=' ')
             # if examp.find(class_='x').text is not None:
@@ -129,25 +129,24 @@ class Scrapper():
             # else:
             #     print(None)
 
-
-
     def print_examples(self, def_):
-        # We need try because 
+        # We need try because
         # there can ben objects corresponding to examples
         try:
             self.print_examples_(def_)
         except AttributeError as e:
             print('0) No examples')
-        except:
+        except BaseException:
             print("Unknow error")
 
     def get_definitions(self):
-        defs = self.page.find('ol', {'class', 'senses_multiple'}).find_all(class_='sense')
+        defs = self.page.find('ol', {'class', 'senses_multiple'}).\
+            find_all(class_='sense')
         print('DEFINITIONS:\n')
         for i, def_ in enumerate(defs):
             print(f"#{i+1}")
             self.print_single_definition(def_)
-            if self.examples_flag == True:
+            if self.examples_flag:
                 print('Examples:')
                 self.print_examples(def_)
                 print()
@@ -155,9 +154,13 @@ class Scrapper():
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description="OXygEN Dictinary package v0.1")
+    parser = argparse.ArgumentParser(
+        description="OXygEN Dictinary package v0.1")
     parser.add_argument("word", help="Enter a word to search")
-    parser.add_argument("--no_examples", action='store_false' ,help="print examples") # can be boolen
+    parser.add_argument(
+        "--no_examples",
+        action='store_false',
+        help="print examples")  # can be boolen
     return parser.parse_args()
 
 
@@ -167,12 +170,12 @@ def main():
     print()  # check
     scrapper = Scrapper(url, headers, args.no_examples)
     # Get the content
-    scrapper.download_data(args.word) 
+    scrapper.download_data(args.word)
     scrapper.get_top_block()
     print('---------------------')
-    scrapper.get_definitions() 
+    scrapper.get_definitions()
     #res = scrapper.search_word(word)
-    #print(res)
+    # print(res)
 
 
 if __name__ == "__main__":
@@ -187,6 +190,6 @@ if __name__ == "__main__":
 # can't handle compex words like take off
 # it should be "take+off"
 
-# make pypi package 
+# make pypi package
 
 # launch from terminal
