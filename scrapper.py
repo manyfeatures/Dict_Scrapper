90fb161468ed6cbc0be5e61550b34f44fd1553ee
manyfeatures <shakurs@mail.ru>
2021-01-31 16:17:39 +0300

    examples have benn added

--- scrapper.py
+++ scrapper.py
@@ -16,7 +16,7 @@ class Scrapper():
         self.url = url
         self.headers = headers
         self.proxy = proxy
-        self.page_content = None
+        self.page = None
         self.def_ = None  # this is just the word
 
     def download_data(self, word):
@@ -28,15 +28,15 @@ class Scrapper():
                      proxies={"http": self.proxy, "https": self.proxy},
                      stream=True  # ?
                      )
-        self.page_content = BeautifulSoup(page.content, 'html.parser')
+        self.page = BeautifulSoup(page.content, 'html.parser')
 
     # def get_definition(self, word):
-    #     self.def_ = self.page_content.find("div", {"class", "top-container"}).\
+    #     self.def_ = self.page.find("div", {"class", "top-container"}).\
     #                      find('h1', {'class', 'headword'}).text
     #     print(self.def_.upper())
 
     def get_top_block(self):
-        tmp = self.page_content.find("div", {"class", "top-container"})
+        tmp = self.page.find("div", {"class", "top-container"})
         #print(tmp)
         #definition
         self.def_  = tmp.find('h1', {'class', 'headword'}).text # make method?
@@ -49,6 +49,45 @@ class Scrapper():
         print(f"Br: {br_pron}")
         am_pron = phone.find(class_="phons_n_am").get_text().strip()
         print(f"Am: {br_pron}")
+
+    def print_definition(self, def_):    
+        # grammar. What does it mean? 
+        print(def_.find('span', {'class', 'grammar'}).get_text(), end = ' ')
+        # labels
+        if def_.find('span', {'class', 'labels'}) is not None:
+            print(def_.find('span', {'class', 'labels'}).get_text())
+        else:
+            print()
+        #defenition
+        print(def_.find('span', {'class', 'def'}).get_text())  
+        print()
+
+    def print_examples(self, def_):
+        # We need try if there is no objects corresponding to examples
+        try:
+            # going through examples
+            for i, examp in enumerate(def_.find(class_='examples').find_all('li')):
+                # As some examples have "specification: text" structure
+                # We need to try to take it
+                try:
+                    print(f"{i}) {examp.find(class_='cf').text}", end=': ')
+                    print(examp.find(class_='x').text) # it has duplicate
+                # If there is no specification then it is skipped
+                except (NameError, AttributeError) as e:
+                    try:
+                        print(example.find(class_='x').text) # it has duplicate
+                    except:
+                        print(None)
+        except AttributeError:
+            print('0) No examples')
+ 
+    def get_definitions(self):
+        defs = self.page.find('ol', {'class', 'senses_multiple'}).find_all(class_='sense')
+        print('DEFINITIONS:\n')
+        for def_ in defs:
+            self.print_definition(def_)
+            self.print_examples(def_)
+            print('------------------------')
 
 
 def parse_args():
@@ -66,7 +105,9 @@ def main():
     word = args.word
     # Get the content
     scrapper.download_data(word) 
-    scrapper.get_top_block() 
+    scrapper.get_top_block()
+    print('---------------------')
+    scrapper.get_definitions() 
     #res = scrapper.search_word(word)
     #print(res)
 
